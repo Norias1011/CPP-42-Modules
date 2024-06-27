75b1acd9195a59c75df9bbdf8114f585bd63d476
Norias1011 <anthonykinzelin@gmail.com>
2024-06-27 16:30:50 +0200

    new

--- cpp_01/ex00/Zombie.cpp
+++ cpp_01/ex00/Zombie.cpp
@@ -6,6 +6,20 @@
 /*   By: akinzeli <akinzeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 13:46:40 by akinzeli          #+#    #+#             */
-/*   Updated: 2024/06/27 13:46:44 by akinzeli         ###   ########.fr       */
+/*   Updated: 2024/06/27 14:13:04 by akinzeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+}
+Zombie::~Zombie()
+{
+}
+
+void Zombie::announce()
+{
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
