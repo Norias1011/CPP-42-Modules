c100086c8731c01e0915d9f8b5278f288ac5cb2f
Norias1011 <anthonykinzelin@gmail.com>
2024-07-01 17:04:35 +0200

    new ex 03

--- cpp_01/ex03/HumanB.cpp
+++ cpp_01/ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: akinzeli <akinzeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:07:02 by akinzeli          #+#    #+#             */
-/*   Updated: 2024/07/01 17:00:55 by akinzeli         ###   ########.fr       */
+/*   Updated: 2024/07/01 17:04:05 by akinzeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string const &name) : _name(name)
+HumanB::HumanB(std::string const &name) : _name(name) , _weapon(NULL)
 {
 }
 
@@ -22,7 +22,12 @@ HumanB::~HumanB()
 
 void HumanB::attack() const
 {
-    std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+    std::cout << this->_name << " attacks with their "; 
+    
+    if (this->_weapon == NULL)
+        std::cout << "big old brain" << std::endl;
+    else
+        std::cout << this->_weapon->getType() << std::endl;
 }
 
 void HumanB::setWeapon(Weapon &name)
