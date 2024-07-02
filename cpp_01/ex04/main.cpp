85265cb2ec34bebf9e63c27798c977d98b1f3750
Norias1011 <anthonykinzelin@gmail.com>
2024-07-02 11:13:52 +0200

    ex04

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,22 +6,52 @@
 /*   By: akinzeli <akinzeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:06:04 by akinzeli          #+#    #+#             */
-/*   Updated: 2024/07/01 17:44:18 by akinzeli         ###   ########.fr       */
+/*   Updated: 2024/07/02 11:13:38 by akinzeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    std::ifstream read_file;
-    std::ofstream write_file;
-    if (ac != 4)
-    {
-        std::cout << "Usage: ./replace <filename> <string1> <string2>" << std::endl;
-        return (1);
-    }
-    
+	if (ac != 4)
+	{
+		std::cout << "Usage: ./sed <filename> <string1> <string2>" << std::endl;
+		return (1);
+	}
+	std::string line;
+	std::string buff;
+	std::string av1(av[1]);
+	std::ifstream read_file(av[1]);
+	std::ofstream write_file(av1 + ".replace");
+	std::size_t position;
+	std::size_t av2size;
+	std::string av2(av[2]);
+	std::string av3(av[3]);
+	av2size = av2.length();
+	if (read_file.is_open())
+	{
+		while (getline(read_file, line))
+		{
+			if (write_file.is_open())
+			{
+				position = line.find(av2);
+				if (position != std::string::npos)
+				{
+					line.erase(position, av2size);
+					line.insert(position, av3);
+					buff.append(line);
+				}
+			}
+		}
+	}
+	if (write_file.is_open())
+	{
+		write_file << buff;
+	}
+	read_file.close();
+	write_file.close();
+	return (0);
 }
